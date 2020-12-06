a4f721317f716f1922b59237abd70b63ea943bb5
Masayoshi Sawada <37367358+masa1203@users.noreply.github.com>
2020-12-07 00:29:20 +0900

    Dev (#2)
    * add/routes
    
    * add/global css
    
    * add/button component
    
    * add/until user-list.tsx

---
+++ src/components/button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+interface ButtonProps {
+  // ボタン名
+  name: string;
+  // ボタンをクリックした時のイベント
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  // 主ボタンか副ボタンかのフラグ
+  primary?: boolean;
+}
+
+class Button extends React.Component<ButtonProps> {
+  render() {
+    return (
+      <ButtonStyle onClick={this.props.onClick} primary={this.props.primary}>
+        {this.props.name}
+      </ButtonStyle>
+    );
+  }
+}
+
+const ButtonStyle = styled.div<{ primary?: boolean }>`
+  display: inline-block;
+  padding: 8px 15px;
+  user-select: none;
+  /* 主ボタンか副ボタンかでボタンのデザインを変更 */
+  background: ${(props) => (props.primary ? "#ffd700" : "#dcdcdc")};
+  border-radius: 5px;
+  &:hover {
+    cursor: pointer;
+    background: ${(props) => (props.primary ? "#ffa500" : "#d3d3d3")};
+  }
+`;
+
+export default Button;
---
+++ src/pages/chat-room-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+class ChatRoomPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>チャットルーム</h1>
+      </div>
+    );
+  }
+}
+
+export default ChatRoomPage;
